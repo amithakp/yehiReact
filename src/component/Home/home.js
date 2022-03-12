4d6de943c6bf6402e60921475666b8d8813b72ec
amithakp <amithapmc97@gmail.com>
2022-03-12 15:36:54 +0530

    modified text

--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className="mainTilecontainer">
                     {/* text on main image */}
                     <div className="heading">
-                        <span className="text-center"> Supply chain management and supply  engineering that plans</span> 
+                        <span className="textHead"> Supply chain management and supply  engineering that plans</span> 
                         <span className="subHeading">In military science, logistics is concerned with maintaining army supply lines while disrupting those of the enemy, since an armed force without resources and transportation is defenseless. Military logistics was already practiced in the ancient world and as the modern military has a significant need for logistics solutions, advanced implementations have been developed. In military logistics, logistics officers manage how and when to move resources to the places they are needed.</span>
                     </div>
                 </div>
