d1fe212b1e8c15c94646a1cfb02575153e1259d0
amithakp <amithapmc97@gmail.com>
2022-03-14 22:56:37 +0530

    modified home.js

--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -6,15 +6,12 @@ const Home = () => {
     return(
         <>
             <Header/>
-            <div className="mainContainer">
+             <div className="mainContainer">
                 <div className="mainTilecontainer">
-                    {/* text on main image */}
-                    <div className="heading">
-                        <div className="textHead"> Supply chain management and supply  engineering that plans</div> 
-                        <div className="subHeading">In military science, logistics is concerned with maintaining army supply lines while disrupting those of the enemy, since an armed force without resources and transportation is defenseless. Military logistics was already practiced in the ancient world and as the modern military has a significant need for logistics solutions, advanced implementations have been developed. In military logistics, logistics officers manage how and when to move resources to the places they are needed.</div>
+                   <h1 className="responsive"> Supply chain management and supply  engineering that plans</h1> 
+                        <p>In military science, logistics is concerned with maintaining army supply lines while disrupting those of the enemy, since an armed force without resources and transportation is defenseless. Military logistics was already practiced in the ancient world and as the modern military has a significant need for logistics solutions, advanced implementations have been developed. In military logistics the modern military has a significant ncient world and as the modern military has a significant need for logistics solutions, advanced implementations have been developed. In military logistics the modern military has a significant</p>
                     </div>
-                </div>
-            </div>
+            </div> 
             {/* four icon section */}
             <div className="tileConatainerIcon  ">
                 <div className=" row">
